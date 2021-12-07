47530a31decc8aa4bb1d2c848ea6fa1d7952db47
Kristof Mattei <864376+Kristof-Mattei@users.noreply.github.com>
2021-12-06 21:28:48 -0800

    fix: day 6 part 1 wip

--- src/day_6/part_1.rs
+++ src/day_6/part_1.rs
@@ -1,13 +1,38 @@
-fn parse_lines(_lines: &[String]) -> Vec<u32> {
-    vec![]
+fn parse_lines(lines: &[String]) -> Vec<u32> {
+    let fishes_nearby = lines[0]
+        .split(',')
+        .map(|f| f.parse::<u32>().unwrap())
+        .collect();
+
+    fishes_nearby
 }
 
-pub fn find_solution() -> u32 {
+fn age_fishes(mut fishes: Vec<u32>) -> Vec<u32> {
+    let mut new_fishes_to_append: usize = 0;
+
+    for fish in &mut fishes {
+        if *fish == 0 {
+            *fish = 6;
+            new_fishes_to_append += 1;
+        } else {
+            *fish -= 1;
+        }
+    }
+
+    fishes.resize(fishes.len() + new_fishes_to_append, 8);
+
+    fishes
+}
+
+pub fn find_solution() -> usize {
     let lines: Vec<String> = include_str!("input.txt").lines().map(Into::into).collect();
 
-    let _parsed = parse_lines(&lines);
+    let mut fishes = parse_lines(&lines);
+    for _ in 0..80 {
+        fishes = age_fishes(fishes);
+    }
 
-    0
+    fishes.len()
 }
 
 #[cfg(test)]
@@ -16,9 +41,33 @@ mod tests {
 
     #[test]
     fn outcome() {
-        assert_eq!(find_solution(), 0);
+        assert_eq!(find_solution(), 395_627);
     }
 
     #[test]
-    fn playground() {}
+    fn playground() {
+        let lines: Vec<String> = vec!["3,4,3,1,2"].iter().map(ToString::to_string).collect();
+
+        let mut fishes: Vec<u32> = parse_lines(&lines);
+
+        for _ in 0..18 {
+            fishes = age_fishes(fishes);
+            println!("{:?}", fishes);
+        }
+
+        assert_eq!(26, fishes.len());
+    }
+
+    #[test]
+    fn playground_2() {
+        let lines: Vec<String> = vec!["3,4,3,1,2"].iter().map(ToString::to_string).collect();
+
+        let mut fishes: Vec<u32> = parse_lines(&lines);
+
+        for _ in 0..80 {
+            fishes = age_fishes(fishes);
+        }
+
+        assert_eq!(5934, fishes.len());
+    }
 }
--- src/day_6/part_2.rs
+++ src/day_6/part_2.rs
@@ -1,13 +1,41 @@
-fn parse_lines(_lines: &[String]) -> Vec<u32> {
-    vec![]
+fn parse_lines(lines: &[String]) -> Vec<u32> {
+    let fishes_nearby = lines[0]
+        .split(',')
+        .map(|f| f.parse::<u32>().unwrap())
+        .collect();
+
+    fishes_nearby
 }
 
-pub fn find_solution() -> u32 {
+fn age_fishes(fishes: Vec<u32>) -> Vec<u32> {
+    let mut new_fishes_to_append: usize = 0;
+
+    let mut new_fishes = Vec::new();
+
+    for fish in fishes {
+        match fish {
+            0 => {
+                new_fishes.push(6);
+                new_fishes_to_append += 1;
+            }
+            x => new_fishes.push(x - 1),
+        }
+    }
+
+    new_fishes.resize(new_fishes.len() + new_fishes_to_append, 8);
+
+    new_fishes
+}
+
+pub fn find_solution() -> usize {
     let lines: Vec<String> = include_str!("input.txt").lines().map(Into::into).collect();
 
-    let _parsed = parse_lines(&lines);
+    let mut fishes = parse_lines(&lines);
+    for _ in 0..80 {
+        fishes = age_fishes(fishes);
+    }
 
-    0
+    fishes.len()
 }
 
 #[cfg(test)]
@@ -16,9 +44,48 @@ mod tests {
 
     #[test]
     fn outcome() {
-        assert_eq!(find_solution(), 0);
+        assert_eq!(find_solution(), 395_627);
     }
 
     #[test]
-    fn playground() {}
+    fn playground() {
+        let lines: Vec<String> = vec!["3,4,3,1,2"].iter().map(ToString::to_string).collect();
+
+        let mut fishes: Vec<u32> = parse_lines(&lines);
+
+        for i in 0..18 {
+            fishes = age_fishes(fishes);
+            println!("After day {}", (i + 1));
+        }
+
+        assert_eq!(26, fishes.len());
+    }
+
+    #[test]
+    fn playground_2() {
+        let lines: Vec<String> = vec!["3,4,3,1,2"].iter().map(ToString::to_string).collect();
+
+        let mut fishes: Vec<u32> = parse_lines(&lines);
+
+        for i in 0..80 {
+            fishes = age_fishes(fishes);
+            println!("After day {}", (i + 1));
+        }
+
+        assert_eq!(5934, fishes.len());
+    }
+
+    #[test]
+    fn playground_3() {
+        let lines: Vec<String> = vec!["3,4,3,1,2"].iter().map(ToString::to_string).collect();
+
+        let mut fishes: Vec<u32> = parse_lines(&lines);
+
+        for i in 0..256 {
+            fishes = age_fishes(fishes);
+            println!("After day {}", (i + 1));
+        }
+
+        assert_eq!(26_984_457_539, fishes.len());
+    }
 }
