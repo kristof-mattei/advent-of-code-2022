b6289900b3293787e48bb585fa2e801de45db5aa
Kristof Mattei <864376+Kristof-Mattei@users.noreply.github.com>
2021-12-07 17:20:05 -0800

    chore: renamed variable

--- src/main.rs
+++ src/main.rs
@@ -28,7 +28,7 @@ fn print_answer(day: u32, part: u32, result: &str) {
 fn main() {
     let mut day: u32 = 1;
 
-    let solutions_2: Vec<Box<dyn Day>> = vec![
+    let solutions: Vec<Box<dyn Day>> = vec![
         Box::new(day_1::Solution {}),
         Box::new(day_2::Solution {}),
         Box::new(day_3::Solution {}),
@@ -39,17 +39,10 @@ fn main() {
         Box::new(day_8::Solution {}),
     ];
 
-    for solution in solutions_2 {
+    for solution in solutions {
         print_answer(day, 1, &solution.part_1().to_string());
         print_answer(day, 2, &solution.part_2().to_string());
 
         day += 1;
     }
-
-    // for (day_solution_part_1, day_solution_part_2) in solutions {
-    //     print_answer(day, 1, &day_solution_part_1);
-    //     print_answer(day, 2, &day_solution_part_2);
-
-    //     day += 1;
-    // }
 }
