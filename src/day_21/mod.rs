440d027853e0df848446b6b4151c02f1240c6d2a
Kristof Mattei <864376+Kristof-Mattei@users.noreply.github.com>
2022-01-13 13:51:08 -0800

    chore: got rid of interior mutability as it isn't needed

--- src/day_21/mod.rs
+++ src/day_21/mod.rs
@@ -1,4 +1,4 @@
-use std::cell::Cell;
+use std::collections::HashMap;
 
 use regex::Regex;
 
@@ -19,51 +19,45 @@ impl DeterministicDie {
     }
 }
 
-struct QuantumDie {}
-
-#[derive(PartialEq, Eq, Debug)]
+#[derive(Clone, PartialEq, Hash, Eq, Debug)]
 struct Player {
     player_number: u32,
-    position: Cell<u32>,
-    score: Cell<u32>,
+    position: u32,
+    score: u32,
 }
 
 impl Player {
     fn new(player_number: u32, start_position: u32) -> Self {
         Self {
             player_number,
-            position: Cell::new(start_position),
-            score: 0.into(),
+            position: start_position,
+            score: 0,
         }
     }
 
-    fn moves(&self, how_much: u32) {
-        self.position.set(self.position.get() + how_much);
+    fn r#move(&mut self, how_much: u32) {
+        self.position += how_much;
 
         loop {
-            let position = self.position.get();
+            let position = self.position;
             if position > 10 {
-                self.position.set(position - 10);
+                self.position = position - 10;
             } else {
                 break;
             }
         }
 
-        self.score.set(self.score.get() + self.position.get());
+        self.score += self.position;
     }
 }
 
-fn anybody_score_above(players: &[Player], above: u32) -> bool {
-    players.iter().any(|p| p.score.get() >= above)
-}
-
-fn play(players: &[Player], until: u32) -> (u32, u32) {
+fn play(mut players: Vec<Player>, until: u32) -> (u32, u32) {
     let mut die = DeterministicDie::new();
 
     let mut dice_rolls = 0;
 
     'outer: loop {
-        for player in players {
+        for player in &mut players {
             let mut rolled = Vec::new();
 
             for _ in 0..3 {
@@ -71,7 +65,7 @@ fn play(players: &[Player], until: u32) -> (u32, u32) {
                 dice_rolls += 1;
             }
 
-            player.moves(rolled.iter().sum());
+            player.r#move(rolled.iter().sum());
             println!(
                 "Player {} rolls {} and moves to place {} for a total score of {}.",
                 player.player_number,
@@ -80,20 +74,17 @@ fn play(players: &[Player], until: u32) -> (u32, u32) {
                     .map(ToString::to_string)
                     .collect::<Vec<_>>()
                     .join("+"),
-                player.position.get(),
-                player.score.get()
+                player.position,
+                player.score
             );
 
-            if anybody_score_above(players, until) {
+            if player.score >= until {
                 break 'outer;
             }
         }
     }
 
-    (
-        players.iter().map(|p| p.score.get()).min().unwrap(),
-        dice_rolls,
-    )
+    (players.iter().map(|p| p.score).min().unwrap(), dice_rolls)
 }
 
 fn parse_lines(lines: &[&str]) -> Vec<Player> {
@@ -112,20 +103,68 @@ fn parse_lines(lines: &[&str]) -> Vec<Player> {
     players
 }
 
+#[derive(Eq, PartialEq, Hash, Clone, Debug)]
 struct Game {
     players: Vec<Player>,
-    next_player: usize,
+    current_player: usize,
 }
 
-fn play_quantum(game: Game, _until: u32) -> Vec<u64> {
-    for (i, player) in game.players.iter().enumerate() {
-        for i in 1_u32..=3 {
-            // foo
-            println!("{:?}: {}", player, i);
-        }
-    }
+fn play_quantum(players: Vec<Player>, until: u32) -> Vec<u128> {
+    let mut results = Vec::new();
+    results.resize(2, 0);
 
-    vec![]
+    let mut games: HashMap<Game, u128> = HashMap::from([(
+        Game {
+            players,
+            current_player: 0,
+        },
+        1,
+    )]);
+
+    loop {
+        // get first value where count > 0
+        // and remove it
+        // check if it's a win
+        // if so, add wins
+        // if not, duplicatae each entry with a count > 0
+        let mut new_games = HashMap::new();
+        if let Some((game, count)) = games.iter_mut().find(|(_, c)| **c > 0) {
+            let next_player = (game.current_player + 1) % game.players.len();
+
+            let c = *count;
+
+            for r in 1..=3 {
+                let mut new_game = game.clone();
+                new_game.players[new_game.current_player].r#move(r);
+
+                if new_game.players[new_game.current_player].score >= until {
+                    results[new_game.current_player] += c;
+                } else {
+                    new_game.current_player = next_player;
+                    new_games.insert(new_game, c);
+                }
+            }
+
+            *count = 0;
+        } else {
+            break;
+        }
+
+        games
+            .iter_mut()
+            .filter(|(_, c)| **c > 0)
+            .for_each(|(_, c)| *c += new_games.values().sum::<u128>());
+
+        for (new_game, count) in new_games {
+            games
+                .entry(new_game)
+                .and_modify(|c| *c += count)
+                .or_insert(count);
+        }
+
+        //println!("{} games to play", games.values().sum::<u64>());
+    }
+    results
 }
 
 pub struct Solution {}
@@ -136,7 +175,7 @@ impl Day for Solution {
 
         let players = parse_lines(&lines);
 
-        let result = play(&players, 1000);
+        let result = play(players, 1000);
 
         PartSolution::U32(result.0 * result.1)
     }
@@ -146,15 +185,9 @@ impl Day for Solution {
 
         let players = parse_lines(&lines);
 
-        let result = play_quantum(
-            Game {
-                players,
-                next_player: 0,
-            },
-            21,
-        );
+        let result = play_quantum(players, 21);
 
-        PartSolution::None
+        PartSolution::U128(*result.iter().max().unwrap())
         // PartSolution::U32(result.0 * result.1)
     }
 }
@@ -194,7 +227,7 @@ mod test {
 
             let players = parse_lines(&example_lines);
 
-            let result = play(&players, 1000);
+            let result = play(players, 1000);
 
             assert_eq!(739_785, result.0 * result.1);
         }
@@ -202,13 +235,26 @@ mod test {
 
     mod part_2 {
         use crate::{
-            day_21::Solution,
+            day_21::{parse_lines, play_quantum, test::get_example, Solution},
             shared::{Day, PartSolution},
         };
 
         #[test]
         fn outcome() {
             assert_eq!((Solution {}).part_2(), PartSolution::None);
+        }
+
+        #[test]
+        fn example() {
+            let example_lines = get_example();
+
+            let players = parse_lines(&example_lines);
+
+            let result = play_quantum(players, 21);
+
+            println!("{:?}", result);
+            assert_eq!(444_356_092_776_315, result[0]);
+            assert_eq!(341_960_390_180_808, result[1]);
         }
     }
 }
--- src/shared/mod.rs
+++ src/shared/mod.rs
@@ -8,6 +8,7 @@ pub enum PartSolution {
     I32(i32),
     U32(u32),
     U64(u64),
+    U128(u128),
     USize(usize),
     Vec(Vec<String>),
     #[allow(dead_code)]
@@ -20,6 +21,7 @@ impl std::fmt::Display for PartSolution {
             PartSolution::I32(x) => x.to_string(),
             PartSolution::U32(x) => x.to_string(),
             PartSolution::U64(x) => x.to_string(),
+            PartSolution::U128(x) => x.to_string(),
             PartSolution::USize(x) => x.to_string(),
             PartSolution::Vec(x) => format!("\n{}", x.join("\n")),
             PartSolution::None => "None".to_owned(),
