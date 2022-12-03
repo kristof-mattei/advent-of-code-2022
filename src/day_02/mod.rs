267f59dbeaf023a3982e9fac1d020d0fc78af086
Kristof Mattei <864376+Kristof-Mattei@users.noreply.github.com>
2022-12-02 17:31:21 -0800

    fix: call right function

--- src/day_02/mod.rs
+++ src/day_02/mod.rs
@@ -152,7 +152,7 @@ mod test {
 
         #[test]
         fn outcome() {
-            assert_eq!(PartSolution::U32(67658), (Solution {}).part_1());
+            assert_eq!(PartSolution::U32(67658), (Solution {}).part_2());
         }
 
         #[test]
