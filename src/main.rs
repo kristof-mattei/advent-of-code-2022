5ed72b07942f55efeba78493c98ab55f5a2b48fd
Kristof Mattei <864376+Kristof-Mattei@users.noreply.github.com>
2022-12-14 10:38:29 -0800

    feat: day 10

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 #![deny(clippy::all)]
 #![warn(clippy::pedantic)]
 #![warn(clippy::cargo)]
-#![allow(clippy::cast_possible_truncation)]
-#![allow(clippy::cast_possible_wrap)]
-#![allow(clippy::cast_sign_loss)]
+// #![allow(clippy::cast_possible_truncation)]
+// #![allow(clippy::cast_possible_wrap)]
+// #![allow(clippy::cast_sign_loss)]
 #![allow(clippy::too_many_lines)]
 #![allow(clippy::let_and_return)]
 
@@ -21,6 +21,7 @@ mod day_06;
 mod day_07;
 mod day_08;
 mod day_09;
+mod day_10;
 
 fn print_answer(day: u32, part: u32, result: &str) {
     println!("Answer to Day {day}, part {part} is ... {result}");
@@ -39,6 +40,7 @@ fn main() {
         Box::new(day_07::Solution {}),
         Box::new(day_08::Solution {}),
         Box::new(day_09::Solution {}),
+        Box::new(day_10::Solution {}),
     ];
 
     for solution in solutions {
